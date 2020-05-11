7db71c1a4e914deeabbeb4ddc672490fcb5c18e0
Renil Tanjeco <renil@targetdisplay.com>
2020-05-11 16:16:59 +0000

    Save initial dev

---
+++ erpnext/selling/report/sales_vat/sales_vat.js
@@ -0,0 +1,35 @@
+frappe.query_reports["Sales VAT"] = {
+
+"filters": [
+
+	{
+
+		"fieldname":"from_date",
+
+		"label": __("From Date"),
+
+		"fieldtype": "Date",
+
+		"width": "80",
+
+		"default": frappe.datetime.month_start()
+
+	},
+
+	{
+
+		"fieldname":"to_date",
+
+		"label": __("To Date"),
+
+		"fieldtype": "Date",
+
+		"width": "80",
+
+		"default": frappe.datetime.month_end()
+
+	}
+
+]
+
+}
